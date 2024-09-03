31d9f3642b651f08ccc54b9d86c512eae0c516a2
Slavikus1 <vyacheslav921@icloud.com>
2024-09-03 15:46:39 +0300

    Добавил обратку ответа пользователя, добавил сохранение ответа и обновление словаря

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,13 +7,40 @@ data class Word(
 )
 
 fun main() {
-    val dictionary = loadDictionary()
+    var dictionary = loadDictionary()
     while (true) {
         val learnedCounter = dictionary.filter { it.learnedNumber >= 3 }
         println("Меню:\n1 - Учить слова\n2 - Статистика\n0 - Выход")
-        val input = readln()
-        when (input) {
-            "1" -> println("Вы нажали 'учить слова'")
+        when (readln()) {
+            "1" -> {
+                val unlearnedNumber = dictionary.filter { it.learnedNumber < 3 }
+                if (unlearnedNumber.isEmpty()) {
+                    println("Вы выучили все слова.")
+                }
+                while (unlearnedNumber.isNotEmpty()) {
+                    val shuffledUnlearned = unlearnedNumber.take(4).shuffled()
+                    val shuffledTranslates = shuffledUnlearned.map { it.translate }
+                    val hiddenWord = shuffledUnlearned.random()
+                    val indexOfHiddenWord = shuffledUnlearned.indexOf(hiddenWord)
+                    println("Как переводится слово ${hiddenWord.original}?")
+                    println(shuffledTranslates)
+                    println("Введите ответ, указав число от 1 до 4. Нажмите 0 для выхода в меню.")
+                    var userAnswer = readln().toIntOrNull()
+                    if (userAnswer == 0) break
+                    while (userAnswer !in 1..4) {
+                        println("Пожалуйста, ответьте указав число от 1 до 4")
+                        userAnswer = readln().toIntOrNull()
+                    }
+                    if (userAnswer == (indexOfHiddenWord + 1)) {
+                        println("Верно!")
+                        hiddenWord.learnedNumber + 1
+                        dictionary = saveDictionary(hiddenWord, dictionary)
+                    } else println("Неверно!")
+                }
+                continue
+
+            }
+
             "2" -> {
                 val learnedPercent = (learnedCounter.size * 100) / dictionary.size
                 println("Количество выученных слов: ${learnedCounter.size}")
@@ -37,4 +64,8 @@ fun loadDictionary(): List<Word> {
         dictionary.add(word)
     }
     return dictionary
+}
+
+fun saveDictionary(word: Word, dictionary: List<Word>): List<Word> {
+    return dictionary.map { it -> if (word.original == it.original) word else it }
 }
