476ea1e38aba5f9a34ebd8806c414af615e9cec9
Slavikus1 <vyacheslav921@icloud.com>
2024-09-16 20:15:21 +0300

    Убрал магические числа и текст

--- src/main/kotlin/LearnWordsTrainer.kt
+++ src/main/kotlin/LearnWordsTrainer.kt
@@ -11,19 +11,22 @@ data class Question(
     val correctAnswer: Word,
 )
 
-class LearnWordsTrainer {
+class LearnWordsTrainer(
+    private val rightAnswerGoal: Int = 3,
+    private val fileName: String = "words.txt",
+) {
     private var question: Question? = null
     private val dictionary = loadDictionary()
 
     fun getStatistics(): Statistics {
-        val learned = dictionary.filter { it.correctAnswersCount >= 3 }.size
+        val learned = dictionary.filter { it.correctAnswersCount >= rightAnswerGoal }.size
         val total = dictionary.size
         val percent = learned * 100 / total
         return Statistics(learned, total, percent)
     }
 
     fun getNextQuestion(): Question? {
-        val notLearnedList = dictionary.filter { it.correctAnswersCount < 3 }
+        val notLearnedList = dictionary.filter { it.correctAnswersCount < rightAnswerGoal }
         if (notLearnedList.isEmpty()) return null
         val questionWords = notLearnedList.take(4).shuffled()
         val correctAnswer = questionWords.random()
@@ -47,7 +50,7 @@ class LearnWordsTrainer {
 
     private fun loadDictionary(): List<Word> {
         val dictionary = mutableListOf<Word>()
-        val wordsFile = File("words.txt")
+        val wordsFile = File(fileName)
         wordsFile.readLines().forEach {
             val splitLine = it.split("|")
             dictionary.add(Word(splitLine[0], splitLine[1], splitLine[2].toIntOrNull() ?: 0))
@@ -56,7 +59,7 @@ class LearnWordsTrainer {
     }
 
     private fun saveDictionary(words: List<Word>) {
-        val wordsFile = File("words.txt")
+        val wordsFile = File(fileName)
         wordsFile.writeText("")
         for (word in words) {
             wordsFile.appendText("${word.questionWord}|${word.translate}|${word.correctAnswersCount}\n")
