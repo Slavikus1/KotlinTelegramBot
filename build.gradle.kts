db7cd3a8fc07dc5b3973fe3e24396efb69ec8dc4
Slavikus1 <vyacheslav921@icloud.com>
2024-10-23 18:25:16 +0300

    KTB-21 публикация на сервере

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("jvm") version "2.0.21"
     kotlin("plugin.serialization") version "2.0.21"
     application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "org.example"
@@ -27,5 +28,5 @@ tasks.withType<KotlinCompile> {
 }
 
 application {
-    mainClass.set("MainKt")
+    mainClass.set("TelegramKt")
 }
